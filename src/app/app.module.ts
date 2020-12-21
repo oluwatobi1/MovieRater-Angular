dcc0fe524e74a5fa4def8a029a674a5d1101c354
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-21 14:42:34 +0100

    setting up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'
-import { PeopleService } from './people.service'
+import { PeopleService } from './people.service';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
