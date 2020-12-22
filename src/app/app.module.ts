8541ca2c15b4b8b716289257c44ea21b41457ff2
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-22 13:25:17 +0100

    removed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { PeopleService } from './people.service';
@@ -19,7 +18,6 @@ import { ChildComponent } from './child/child.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
        
