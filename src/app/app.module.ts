423bce7a0534f2b29994d3ebef39e70b6e2f9b67
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-22 13:28:28 +0100

    Safely removed second component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { PeopleService } from './people.service';
 import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
   ],
   imports: [
     BrowserModule,
