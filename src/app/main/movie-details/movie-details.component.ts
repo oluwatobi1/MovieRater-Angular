0b5658a783c230cb0050acf834f7706f0b25f4e4
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-23 17:00:02 +0100

    Fixed null bug on Movie click

--- src/app/main/movie-details/movie-details.component.ts
+++ src/app/main/movie-details/movie-details.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  @Input() currentMovie = []
+  @Input() currentMovie;
 
   constructor() { }
 
