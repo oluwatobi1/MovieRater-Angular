007370c26113fa65b77d6af7ce705d4ddf8c1c00
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-23 13:27:56 +0100

    simple component styling

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 const routes: Routes=[
   {path:'movies', component:MainComponent}
 ]
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MovieListComponent, MovieDetailsComponent],
   imports: [RouterModule.forChild(routes)  ],
   exports:[RouterModule]
 })
